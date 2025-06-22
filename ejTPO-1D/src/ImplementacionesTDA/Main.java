5b9d33591940c665e9158049155f49500befcad6
tbaiass <tbercovich@uade.edu.ar>
2025-06-22 16:56:26 -0300

    Add files via upload

---
+++ ejTPO-1D/src/ImplementacionesTDA/Main.java
@@ -0,0 +1,23 @@
+package ImplementacionesTDA;
+
+public class Main {
+    public static void main(String[] args) {
+        ConjuntoConRepetidos conjunto = new ConjuntoConRepetidos();
+        conjunto.inicializar();
+
+        conjunto.agregar(10);
+        conjunto.agregar(5);
+        conjunto.agregar(10);
+        conjunto.agregar(7);
+        conjunto.agregar(5);
+        conjunto.agregar(5);
+
+        conjunto.mostrar();
+
+        conjunto.eliminar(5);  // Resta cantidad
+        conjunto.eliminar(7);  // Elimina nodo (cantidad = 1)
+        conjunto.eliminar(100); // No existe
+
+        conjunto.mostrar();
+    }
+}
